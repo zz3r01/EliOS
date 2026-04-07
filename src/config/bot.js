2c7ae75c3c6a1861919a04c02c2a9324de42e3e1
zz3r01 <danilbrovkin128@gmail.com>
2026-04-07 13:07:33 +0200

    Update bot activity name to Russian translation

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Сделано Производственной Службой",
         // Activity type number (0 = Playing).
         type: 0, 
       },
