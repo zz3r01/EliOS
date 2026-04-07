1f6d3b4da6d90c6214297b3751480764aa474933
zz3r01 <danilbrovkin128@gmail.com>
2026-04-07 17:19:29 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "/help | Создано Производственной Службой",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 1, 
       },
     ],
   },
